e15875df5872d98fd1d3218a3618897ec382eb9d
Alex14101987 <freedom20011987@gmail.com>
2023-09-15 10:39:54 +0700

    add PIXHAWK data, fix check folder sizechange time_format

--- video_cam.py
+++ video_cam.py
@@ -1,7 +1,8 @@
 # mavproxy.py --master=/dev/ttyACM0 --out=udp:127.0.0.1:14550 --out=udp:127.0.0.1:14551 --daemon 2>/dev/null 1>&2 &
+# v4l2-ctl -d /dev/video6 --list-formats-ext
 
 import cv2
-import os
+import os, shutil
 import json
 import time
 from datetime import datetime
@@ -20,18 +21,17 @@ HEIGHT = 480
 # WIDTH = 1920
 # HEIGHT = 1080
 FPS = 30
-FOURCC = cv2.VideoWriter_fourcc(*'MJPG')
+# FOURCC = cv2.VideoWriter_fourcc(*'MJPG')
 ALPHA = 0.98
 DT = 1 / 400_000
 
-
 # AUTO_EXPOSURE = 1
 # EXPOSURE = 50
-# FOURCC = cv2.VideoWriter_fourcc(*'YUYV')
+FOURCC = cv2.VideoWriter_fourcc(*'YUYV')
 
 class Video():
     def __init__(self, save_dir):
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(4)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
         self.cap.set(cv2.CAP_PROP_FPS, FPS)
@@ -44,6 +44,7 @@ class Video():
         self.metadata = {}
         self.roll = None
         self.pitch = None
+        self.yaw = 0
         self.roll_P = None
         self.pitch_P = None
         self.yaw = None
@@ -58,11 +59,21 @@ class Video():
         self.acc_x = None
         self.acc_y = None
         self.acc_z = None
+        self.gyro_x_P = None
+        self.gyro_y_P = None
+        self.gyro_z_P = None
+        self.mag_x_P = None
+        self.mag_y_P = None
+        self.mag_z_P = None
         self.acc_x_IMU = None
         self.acc_y_IMU = None
         self.acc_z_IMU = None
         self.roll_P_G = None
         self.pitch_P_G = None
+        self.yaw_P_G = None
+        self.gyro_x_IMU = None
+        self.gyro_y_IMU = None
+        self.gyro_z_IMU = None
 
     def start(self):
         try:
@@ -82,11 +93,11 @@ class Video():
             # cv2.putText(frame, str(self.count), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             self.metadata = {
                 'frame_id': self.count,
-                'timestamp': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')}
+                'timestamp': datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f')}
             self.metadata.update({
-                                'roll_P': self.roll_P,
-                                'pitch_P': self.pitch_P,
-                                'yaw_P': self.yaw_P,
+                                'roll_P_G': self.roll_P_G,
+                                'pitch_P_G': self.pitch_P_G,
+                                'yaw_P_G': self.yaw_P_G,
                                 'heading': self.heading,
                                 'groundspeed': self.groundspeed,
                                 'latitude': self.latitude,
@@ -97,15 +108,24 @@ class Video():
                                 'acc_x_P': self.acc_x,
                                 'acc_y_P': self.acc_y,
                                 'acc_z_P': self.acc_z,
-
+                                'gyro_x_P': self.gyro_x_P,
+                                'gyro_y_P': self.gyro_y_P,
+                                'gyro_z_P': self.gyro_z_P,
+                                'mag_x_P': self.mag_x_P,
+                                'mag_x_P': self.mag_x_P,
+                                'mag_z_P': self.mag_z_P,
                                 })
             self.metadata.update({
                 # roll и pitch поменяны местами так датчик закреплен боком
                                   'roll_IMU': self.pitch,
                                   'pitch_IMU': self.roll,
+                                  # 'yaw_IMU': self.yaw,
                                   'acc_x_IMU': self.acc_x_IMU,
                                   'acc_y_IMU': self.acc_y_IMU,
-                                  'acc_z_IMU': self.acc_z_IMU
+                                  'acc_z_IMU': self.acc_z_IMU,
+                                  'gyro_x_IMU': self.gyro_x_IMU,
+                                  'gyro_y_IMU': self.gyro_y_IMU,
+                                  'gyro_z_IMU': self.gyro_z_IMU
                                 })
             self.count += 1
             # print(self.metadata_gps)
@@ -121,13 +141,15 @@ class Video():
 
             # print(math.sqrt(accel['x']**2+accel['y']**2+accel['z']**2))
 
-            roll_acc = math.degrees(math.atan2( math.sqrt(accel['x'] ** 2 + accel['z'] ** 2), accel['y']))
+            roll_acc = math.degrees(math.atan2(math.sqrt(accel['x'] ** 2 + accel['z'] ** 2), accel['y']))
             if accel['z'] > 0:
                 roll_acc *= -1
             pitch_acc = math.degrees(math.atan2(-accel['x'], math.sqrt(accel['y'] ** 2 + accel['z'] ** 2)))
             self.acc_x_IMU, self.acc_y_IMU, self.acc_z_IMU = round(accel['x']*1000), round(accel['y']*1000), round(accel['z']*1000)
+            self.gyro_x_IMU, self.gyro_y_IMU, self.gyro_z_IMU = gyro['x'], gyro['y'], gyro['z']
             self.roll = ALPHA * (roll_acc + math.degrees(gyro['x']) * DT) + (1 - ALPHA) * roll_acc
             self.pitch = ALPHA * (pitch_acc + math.degrees(gyro['y']) * DT) + (1 - ALPHA) * pitch_acc
+            # self.yaw += gyro['z']*DT
 
             # mean_lst_roll.append(roll_acc)
             # mean_lst_pitch.append(pitch_acc)
@@ -137,6 +159,7 @@ class Video():
             #     mean_lst_roll, mean_lst_pitch = [], []
 
     def run_GPS(self):
+        # Подключение к устройству
         # master = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600)
 
         while True:
@@ -147,7 +170,7 @@ class Video():
                 msg_attitude = master.recv_match(type='ATTITUDE', blocking=True, timeout=0.001)
                 self.roll_P_G = msg_attitude.roll
                 self.pitch_P_G = msg_attitude.pitch
-                self.yaw_P = msg_attitude.yaw
+                self.yaw_P_G = msg_attitude.yaw
             except:
                 continue
 
@@ -174,6 +197,12 @@ class Video():
                 self.acc_x = msg_scaled_imu.xacc
                 self.acc_y = msg_scaled_imu.yacc
                 self.acc_z = msg_scaled_imu.zacc
+                self.gyro_x_P = msg_scaled_imu.xgyro
+                self.gyro_y_P = msg_scaled_imu.ygyro
+                self.gyro_z_P = msg_scaled_imu.zgyro
+                self.mag_x_P = msg_scaled_imu.xmag
+                self.mag_y_P = msg_scaled_imu.ymag
+                self.mag_z_P = msg_scaled_imu.zmag
                 self.roll_P = math.degrees(math.atan2(self.acc_y, math.sqrt(self.acc_x ** 2 + self.acc_z ** 2)))
                 self.pitch_P = -(math.degrees(math.atan2(-self.acc_x, math.sqrt(self.acc_y ** 2 + self.acc_z ** 2))))
             except:
@@ -183,14 +212,22 @@ class Video():
 def video_write(save_dir):
     video = Video(save_dir)
     video.start()
-    folder_count = math.ceil(len(os.listdir(save_dir)))
+    # folder_count = math.ceil(len(os.listdir(save_dir)))
+    folders = [f for f in os.listdir(save_dir) if os.path.isdir(os.path.join(save_dir, f))]
+    if not folders:
+        folder_count = 0
+    else:
+        for i in folders:
+            if not i.isdigit():
+                folders.remove(i)
+        max_folder = max(folders, key=lambda x: int(x))
+        folder_count = int(max_folder) + 1
     os.makedirs(f'{save_dir}/{folder_count}')
     # os.makedirs(f'{save_dir}/{folder_count}/frames/')
     count = 0
-    # FPS *= 2.5
     video_writer = cv2.VideoWriter(
         f'videos/{folder_count}/{count}.avi',
-        FOURCC,
+        cv2.VideoWriter.fourcc(*"FFV1"),
         FPS,
         (WIDTH, HEIGHT)
     )
@@ -198,38 +235,20 @@ def video_write(save_dir):
     frame_count = 0
     cur_count = 0
     while True:
-
-        # проверка на максимальный размер папки, если больше 20 Гб, то начинают удаляться старые файлы
-        folder_size = sum(os.path.getsize(os.path.join(save_dir, f)) for f in os.listdir(save_dir) if
-                          os.path.isfile(os.path.join(save_dir, f)))
-        if folder_size > (20 * 1024 * 1024 * 1024):
-            oldest_file = min(os.listdir(save_dir), key=lambda f: os.path.getctime(os.path.join(save_dir, f)))
-            os.remove(os.path.join(save_dir, oldest_file))
-
         # если счетчик изменился, то дописывем фрейм и добавляем метаданные
         if cur_count < video.count:
-            video_writer.write(video.frame)
             metadata.append(video.metadata)
+            video_writer.write(video.frame)
             cur_count = video.count
             frame_count += 1
-            # try:
-            #     print(
-            #         'roll_P', round(video.roll_P, 2),
-            #         'pitch_P', round(video.pitch_P, 2),
-            #         'acc_x_P', round(video.acc_x, 2),
-            #         'acc_y_P', round(video.acc_y, 2),
-            #         'acc_z_P', round(video.acc_z, 2)
-            #         )
-            # except:
-            #     continue
-            
-        # каждые 300 фреймов релиз видео, запись метаданных в json
-        if frame_count >= FPS * 10:
+
+        # каждые 100 фреймов релиз видео, запись метаданных в json
+        if frame_count >= 100:
             print('RELEASE', count, 'FPS', video.cap.get(cv2.CAP_PROP_FPS), 'SIZE', (WIDTH, HEIGHT))
-            video_writer.release()
-            os.sync()
             with open(f'videos/{folder_count}/{count}.json', 'w') as f:
                 json.dump(metadata, f)
+            video_writer.release()
+            os.sync()
 
             # # запись фрейма с метаданными
             # im = Image.fromarray(video.frame)
@@ -248,10 +267,28 @@ def video_write(save_dir):
             video_writer = None
             video_writer = cv2.VideoWriter(
                 f'videos/{folder_count}/{count}.avi',
-                FOURCC,
+                cv2.VideoWriter.fourcc(*"FFV1"),
                 FPS,
                 (WIDTH, HEIGHT)
             )
+
+            # проверка на максимальный размер папки, если больше 20 Гб, то начинают удаляться старые файлы
+            total_size = 0
+            for dirpath, dirnames, filenames in os.walk(save_dir):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    total_size += os.path.getsize(fp)
+            if total_size > (20 * 1024 * 1024 * 1024):
+                oldest_file = None
+                oldest_file_ctime = None
+                for dirpath, dirnames, filenames in os.walk(save_dir):
+                    for f in filenames:
+                        fp = os.path.join(dirpath, f)
+                        current_file_ctime = os.path.getctime(fp)
+                        if oldest_file is None or current_file_ctime < oldest_file_ctime:
+                            oldest_file = fp
+                            oldest_file_ctime = current_file_ctime
+                os.remove(os.path.join(save_dir, oldest_file))
 
 if __name__ == '__main__':
     save_dir = 'videos'
