67b0f87efbfd57947b9e0064630d42c842e29875
alex14101987 <freedom20011987@gmail.com>
2023-11-23 15:14:42 +0700

    read_frames_with_metadata.py reads newest file now

--- read_frame_with_metadata.py
+++ read_frame_with_metadata.py
@@ -1,7 +1,17 @@
 from PIL import Image
+import os
 
+def get_newest_file(directory):
+    files = [f[:-4] for f in os.listdir(os.path.join("/home/orangepi/videos", directory, 'frames'))]
+    max_files = sorted(files, key=int)[-1]
+    print('FILE',max_files)
+    return max_files
 
-file_path = 'videos/43/frames/1698793477596341818.png'
-im = Image.open(file_path)
+subfolders = [f for f in os.listdir("/home/orangepi/videos") if os.path.isdir(os.path.join("/home/orangepi/videos", f))]
+max_subfolder = sorted(subfolders, key=int)[-1]
+print(max_subfolder)
+newest_file = os.path.join("/home/orangepi/videos", max_subfolder, 'frames', get_newest_file(os.path.join("/home/orangepi/videos", max_subfolder)))
+print(newest_file)
+im = Image.open(newest_file+'.png')
 metadata = im.info['metadata']
 print(metadata)
